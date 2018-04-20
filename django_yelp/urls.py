5e1cb2fa23d7541cab4dad9a47f1ceb089510da7
Aaditya Panikath <aadityarajeev@gmail.com>
2018-04-20 14:56:25 -0700

    Changes

--- django_yelp/urls.py
+++ django_yelp/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-from django_yelp import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='yelp_index', permanent=False)),
     path('admin/', admin.site.urls),
     path('yelpme/', include('yelp.urls')),
 ]
--- yelp/urls.py
+++ yelp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='yelp_index'),
     path('api/cuisine_autocomplete/', views.get_cuisine_autocomplete, name='cuisine_autocomplete'),
 ]
